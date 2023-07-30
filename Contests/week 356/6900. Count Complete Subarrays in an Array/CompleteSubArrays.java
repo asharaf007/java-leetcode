e8828e9232a5e0e174da489fe034b9dcb39cd6bf
asharaf007 <718romanempire@gmail.com>
2023-07-30 08:52:59 +0530

    added weekly contest 356 solutions

---
+++ Contests/week 356/6900. Count Complete Subarrays in an Array/CompleteSubArrays.java
@@ -0,0 +1,34 @@
+package Java.leetcodeSolution.Contests.week355;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+//https://leetcode.com/contest/weekly-contest-356/problems/count-complete-subarrays-in-an-array/
+public class CompleteSubArrays {
+    public int countCompleteSubarrays(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int unique=set.size();
+        int count,j,ans=0;
+        for(int i=0;i<=nums.length-unique;i++){
+            count=0;
+            set.clear();
+            for(j=i;j<nums.length;j++){
+                if(count==unique){
+                    ans+=nums.length-j+1;
+                    break;
+                }
+                if(!set.contains(nums[j])){
+                    count++;
+                    set.add(nums[j]);
+                }
+            }
+            if(j==nums.length&&count==unique){
+                ans+=1;
+            }
+        }
+        return ans;
+    }
+}
