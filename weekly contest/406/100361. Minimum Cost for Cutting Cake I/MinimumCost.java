17a7d357f3b78e9bf8abd5a74b1eb9daf0500d3a
md asharaf <718romanempire@gmail.com>
2024-07-19 01:40:27 +0530

    feat: Implement minimumCost method for cutting cake

--- weekly contest/406/100361. Minimum Cost for Cutting Cake I/MinimumCost.java
+++ weekly contest/406/100361. Minimum Cost for Cutting Cake I/MinimumCost.java
@@ -1,55 +1,30 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.concurrent.PriorityBlockingQueue;
 
+import java.util.PriorityQueue;
+
+//https://leetcode.com/problems/minimum-cost-for-cutting-cake-i/
 public class MinimumCost {
-    int cost;
 
     public int minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
-        cost = 0;
-        makeCuts(0, 0, m, n);
-        return cost;
-    }
-
-    private void makeCuts(int rowStart, int colStart, int row, int col, int[] horizontalCut, int[] verticalCut) {
-        if (row == 1 && col == 1) {
-            return;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (int i = 0; i < horizontalCut.length; i++) {
+            pq.add(new int[] { horizontalCut[i], 1 });
         }
-        if (row == 1) {
-            for (int i = colStart; i < colStart + col - 1; i++) {
-                cost += verticalCut[i];
-            }
-            return;
-        } else if (col == 1) {
-            for (int i = rowStart; i < rowStart + row - 1; i++) {
-                cost += horizontalCut[i];
-            }
-            return;
-        } else {
-            int rowMax = 0, rowIndex = 0, colMax = 0, colIndex = 0;
-            for (int i = rowStart; i < rowStart + row - 1; i++) {
-                if (horizontalCut[i] > rowMax) {
-                    rowMax = horizontalCut[i];
-                    rowIndex = i;
-                }
-            }
-            for (int i = colStart; i < colStart + col - 1; i++) {
-                if (verticalCut[i] > colMax) {
-                    colMax = verticalCut[i];
-                    colIndex = i;
-                }
-            }
-            if (rowMax * col < colMax * row) {
-                cost += rowMax;
-                makeCuts(rowStart, colStart, rowIndex - rowStart + 1, col, horizontalCut, verticalCut);
-                makeCuts(rowIndex, colStart, row - rowIndex + rowStart, col, horizontalCut, verticalCut);
+        for (int i = 0; i < verticalCut.length; i++) {
+            pq.add(new int[] { verticalCut[i], 0 });
+        }
+        int cost = 0;
+        int hcuts = 1;
+        int vcuts = 1;
+        while (!pq.isEmpty()) {
+            int[] cut = pq.poll();
+            if (cut[1] == 1) {
+                cost += cut[0] * vcuts;
+                hcuts++;
             } else {
-                cost += colMax;
-                makeCuts(rowStart, colStart, row, colIndex - colStart + 1, horizontalCut, verticalCut);
-                makeCuts(rowStart, colIndex, col, col - colIndex + colStart, horizontalCut, verticalCut);
+                cost += cut[0] * hcuts;
+                vcuts++;
             }
         }
-
+        return cost;
     }
 }
