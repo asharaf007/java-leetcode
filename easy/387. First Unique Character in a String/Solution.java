05049c79faf66d6128079de0c4bc1ae33304e98e
md asharaf <718romanempire@gmail.com>
2024-08-16 23:48:52 +0530

    feat: Add Solution class for First Unique Character in a String problem

---
+++ easy/387. First Unique Character in a String/Solution.java
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/first-unique-character-in-a-string/
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Queue<Character> q=new LinkedList<>();
+        int[] firstOccurence=new int[26];
+        Arrays.fill(firstOccurence,-1); // -1 means character yet to be seen 
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!q.isEmpty()&&q.peek()==c){
+                q.remove();
+                while(!q.isEmpty()&&firstOccurence[q.peek()-'a']==-2){
+                    q.remove();
+                }
+            }
+            if(firstOccurence[c-'a']==-1){
+                q.add(c);
+                firstOccurence[c-'a']=i;
+            }else{
+                firstOccurence[c-'a']=-2; // -2 means character has been seen more than 1 time
+            }
+        }
+        // while(!q.isEmpty()){
+        //     int i=q.poll()-'a';
+        //     if(firstOccurence[i]>=0){
+        //         return firstOccurence[i];
+        //     }
+        // }
+        return q.isEmpty()?-1:firstOccurence[q.peek()-'a'];
+    }
+}
