78d007e5fe77c7374a5ff03e536b0f45c4e1205b
md asharaf <718romanempire@gmail.com>
2024-08-27 00:28:25 +0530

    feat: Implement solution for Assign Cookies problem
    This commit adds a new Solution class for the Assign Cookies problem. The solution uses the greedy algorithm to find the maximum number of content children. It sorts the arrays of children and cookies, and then iterates through them to find the matches. The count of content children is returned as the result.
    
    feat: Refactor solution for Add Two Numbers II problem
    
    This commit refactors the Solution class for the Add Two Numbers II problem. The code is modified to reverse the input linked lists and perform the addition in reverse order. It uses a carry variable to handle the carry-over during addition. The result is then reversed back to the original order and returned as the output linked list.

--- medium/445. Add Two Numbers II/AddTwoNumbers2.java
+++ medium/445. Add Two Numbers II/AddTwoNumbers2.java
@@ -1,45 +1,42 @@
-package Java.leetcodeSolution.medium;
-
-import Java.OOPS.customDataStructures.linkedList.ListNode;
-
 //https://leetcode.com/problems/add-two-numbers-ii/
 public class AddTwoNumbers2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        ListNode head1 = l1;
-        ListNode head2 = l2;
-        while (head1 != null) {
-            sb1.append((char) (head1.val + '0'));
-            head1 = head1.next;
+        l1=reverseList(l1);
+        l2=reverseList(l2);
+        ListNode head=new ListNode(0);
+        ListNode temp=head;
+        int carry=0;
+        while(l1!=null||l2!=null){
+            if(l1==null){
+                carry+=l2.val;
+            }else if(l2==null){
+                carry+=l1.val;
+            }else{
+                carry+=l1.val+l2.val;
+            }
+            temp.next=new ListNode(carry%10);
+            carry/=10;
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
+            temp=temp.next;
         }
-        while (head2 != null) {
-            sb1.append((char) (head2.val + '0'));
-            head2 = head2.next;
+        if(carry>0){
+            temp.next=new ListNode(carry);
         }
-        StringBuilder sb = sb1.length() > sb2.length() ? addStrings(sb1, sb2, 0) : addStrings(sb2, sb1, 0);
-        ListNode ans = new ListNode(1);
-        ListNode temp = ans;
-        for (int i = 0; i < sb.length(); i++) {
-            temp.next = new ListNode(sb.charAt(i) - '0');
-            temp = temp.next;
-        }
-        return ans.next;
+        return reverseList(head.next);
     }
-
-    private static StringBuilder addStrings(StringBuilder num1, StringBuilder num2, int carry) {
-        if (num1.isEmpty()) {
-            StringBuilder sb = new StringBuilder();
-            return carry == 0 ? sb : sb.append("1");
+    private ListNode reverseList(ListNode head){
+        if(head==null||head.next==null){
+            return head;
         }
-        if (num2.isEmpty()) {
-            if (carry == 0) return num1;
-            return addStrings(num1, new StringBuilder("1"), 0);
-        }
-        int c = num1.charAt(num1.length() - 1) + num2.charAt(num2.length() - 1) + carry - 96;
-        carry = c / 10;
-        c = c % 10;
-        char ch = (char) (c + '0');
-        return addStrings(num1.deleteCharAt(num1.length() - 1), num2.deleteCharAt(num2.length() - 1), carry).append(ch);
+        ListNode next=head.next;
+        ListNode newHead=reverseList(next);
+        next.next=head;
+        head.next=null;
+        return newHead;
     }
 }
